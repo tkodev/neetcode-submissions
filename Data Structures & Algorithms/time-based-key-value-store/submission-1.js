2cc2e76ad9cb9133835968ca89e62dac98c54cc3
Tony Ko <tony@tko.dev>
2026-07-01 17:16:21 -0400

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.js
@@ -0,0 +1,44 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if (!this.keyStore.has(key)) {
+            this.keyStore.set(key, [[timestamp, value]])
+        } else {
+            this.keyStore.get(key).push([timestamp, value])
+        }
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        const values = this.keyStore.get(key) || []
+        
+        let left = 0
+        let right = values.length - 1
+        let result = ""
+
+        while (left <= right) { 
+            const mid = Math.floor((left + right) / 2)
+            const [midTimestamp, midData] = values[mid] ?? []
+            if (midTimestamp <= timestamp) {
+                result = midData
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        return result
+    }
+}
